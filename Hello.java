36419f8c9869a6bccf47f057d7b42e578acafa1d
tsupriya <85955832+tsupriya@users.noreply.github.com>
2021-06-15 22:32:08 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args){
+        printHello();
+
+    }
+    public static void printHello(){
+        for(int i = 0; i<10; i++) System.out.println("Hello World");
+    }
+
+}
